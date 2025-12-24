8f99748434ca819dc79bbe0fa1190f0897702f4d
crestbharatphalak <bharat.p@crestinfosystems.com>
2025-12-24 17:26:28 +0530

    Synchronizing job Sec AI Test Job from AWS Glue at 2025/12/24 11:56:28

--- sec_ai_jobs/Sec AI Test Job.py
+++ sec_ai_jobs/Sec AI Test Job.py
@@ -9,8 +9,6 @@ if __name__ == "__main__":
     print("Starting Glue Code")
     try:
         main()
-        job.commit()
-        
         print("Ending Glue Code")
     except Exception as e:
         logger.exception("Glue job failed")
