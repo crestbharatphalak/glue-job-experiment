fd6d312b59e1b1d8ea2cd295a8fa9897b07d86b8
crestbharatphalak <bharat.p@crestinfosystems.com>
2025-12-24 17:33:14 +0530

    Synchronizing job Sec AI Test Job from AWS Glue at 2025/12/24 12:03:14

--- sec_ai_jobs/Sec AI Test Job.py
+++ sec_ai_jobs/Sec AI Test Job.py
@@ -4,6 +4,17 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from func_one import func_one
+from func_two import func_two
+
+
+def main():
+    print("========== GLUE GITHUB TEST START ==========")
+
+    func_one()
+    func_two()
+
+    print("========== GLUE GITHUB TEST END ==========")
 
 if __name__ == "__main__":
     print("Starting Glue Code")
